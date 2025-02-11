3c49aae80757f0b2643c5d1e78ef278d72ed608f
iambasantarai <iambasantarai@gmail.com>
2025-02-11 19:13:28 +0545

    Update multi monitor setup script

--- .screenlayout/multi-monitor.sh
+++ .screenlayout/multi-monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --brightness 0.70 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output HDMI-1 --brightness 0.85 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL-1 --off
