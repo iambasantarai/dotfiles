8c5a637df0c72f8b3e339b260fdda4a1eb068c73
Basanta Rai <developer.basanta@gmail.com>
2025-10-24 18:53:13 +0545

    Spawn a new instance of wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,36 +2,41 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config = {
-    -- General
-    initial_cols = 120,
-    initial_rows = 28,
-    font_size = 14,
-    line_height = 1.2,
-    font = wezterm.font("JetBrains Maple Mono"),
-    color_scheme = 'Gruber (base16)',
-    window_decorations = "RESIZE",
-    window_close_confirmation = "NeverPrompt",
-    enable_tab_bar = false,
-    window_padding = {
-        left = 4,
-        right = 0,
-        top = 2,
-        bottom = 0
-    },
+	-- General
+	initial_cols = 120,
+	initial_rows = 28,
+	font_size = 14,
+	line_height = 1.2,
+	font = wezterm.font("JetBrains Maple Mono"),
+	color_scheme = "Gruber (base16)",
+	window_decorations = "RESIZE",
+	window_close_confirmation = "NeverPrompt",
+	enable_tab_bar = false,
+	window_padding = {
+		left = 4,
+		right = 0,
+		top = 2,
+		bottom = 0,
+	},
 
-    -- Key bindings 
-    keys = {
-        {
-            key = "w",
-            mods = "CMD",
-            action = wezterm.action.CloseCurrentPane { confirm = false },
-        },
-        {
-            key = "f",
-            mods = "CTRL",
-            action = wezterm.action.ToggleFullScreen,
-        },
-    }
+	-- Key bindings
+	keys = {
+		{
+			key = "w",
+			mods = "CMD",
+			action = wezterm.action.CloseCurrentPane({ confirm = false }),
+		},
+		{
+			key = "f",
+			mods = "CTRL",
+			action = wezterm.action.ToggleFullScreen,
+		},
+		{
+			key = "Return",
+			mods = "CTRL",
+			action = wezterm.action.SpawnWindow,
+		},
+	},
 }
 
 return config
